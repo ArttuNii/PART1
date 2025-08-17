91318879591f373ef54407ef849a04422818ae6c
ArttuNii <arttunii@uef.fi>
2025-08-17 18:25:49 +0300

    testi

--- teht3.9/teht2.6/index.js
+++ teht3.9/teht2.6/index.js
@@ -35,7 +35,7 @@ let persons = [
     }
 ]
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src', 'dist')));
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -94,8 +94,8 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
